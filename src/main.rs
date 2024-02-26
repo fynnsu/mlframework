80d5490faeb0d80d06140713fe0e6d12078335c5
Fynn Schmitt-Ulms <fynnsu@outlook.com>
2024-02-26 16:46:50 -0500

    Store tensor data behind RC pointer

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ use mlframework::tensor::Tensor;
 fn main() {
     let x = Tensor::new(vec![3, 4, 5]);
     let y = Tensor::new(vec![1, -2, 1]);
-    let s = x + y;
-    println!("{:#?}", s);
+    let s = x.clone() + y.clone();
+    println!("{:#?} + {:#?} = {:#?}", x, y, s);
 }
--- src/tensor.rs
+++ src/tensor.rs
@@ -1,34 +1,38 @@
-use std::{fmt, ops::Add};
+use std::{ops::Add, rc::Rc};
 
 #[derive(Debug)]
 pub struct Tensor<T> {
-    pub data: Vec<T>,
+    pub _data: Rc<Vec<T>>,
 }
-
-// impl<T: fmt::Display> fmt::Display for Tensor<T> {
-//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-//         write!(f, "Tensor({})", self.data)
-//     }
-// }
 
 impl<T> Tensor<T> {
     pub fn new(data: Vec<T>) -> Self {
-        Self { data }
+        Self {
+            _data: Rc::new(data),
+        }
+    }
+}
+
+impl<T> Clone for Tensor<T> {
+    fn clone(&self) -> Self {
+        Self {
+            _data: Rc::clone(&self._data),
+        }
     }
 }
 
 impl<T: Add<Output = T> + Copy> Add for Tensor<T> {
     type Output = Self;
-
-    fn add(self, other: Self) -> Self {
-        assert!(self.data.len() == other.data.len());
-        Self {
-            data: self
-                .data
-                .iter()
-                .zip(other.data.iter())
-                .map(|(a, b)| *a + *b)
-                .collect(),
+    fn add(self, other: Self) -> Tensor<T> {
+        assert!(self._data.len() == other._data.len());
+        Tensor {
+            _data: Rc::new(
+                self._data
+                    .iter()
+                    .zip(other._data.iter())
+                    .map(|(a, b)| *a + *b)
+                    .collect(),
+            ),
         }
     }
 }
