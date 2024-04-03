5f4a9386f4480464bab178968c7ea9b07101d194
Fynn Schmitt-Ulms <fynnsu@outlook.com>
2024-04-03 17:24:02 -0400

    clippy

--- src/optim.rs
+++ src/optim.rs
@@ -1,5 +1,5 @@
 use crate::dtype::Dtype;
 
 pub trait Optimizer {
-    fn compute<T: Dtype>(&mut self, tid: usize, t_value: &Vec<T>, t_grad: &Vec<T>) -> Vec<T>;
+    fn compute<T: Dtype>(&mut self, tid: usize, t_value: &[T], t_grad: &[T]) -> Vec<T>;
 }
